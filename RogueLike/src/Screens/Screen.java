c60f243f608d6d093a2780e681ec3ea7db176723
JorViteri <rorschacht14@gmail.com>
2019-07-23 17:47:09 +0200

    little cleaning in Screen interface

--- RogueLike/src/Screens/Screen.java
+++ RogueLike/src/Screens/Screen.java
@@ -8,7 +8,7 @@ import asciiPanel.AsciiPanel;
 
 public interface Screen {
 
-	//public void displayOutput(SGPane terminal);
+	
 	public void displayOutput(AsciiPanel terminal, JTextArea textArea1, JTextArea textArea2);
 	
 	public Screen respondToUserInput(KeyEvent key);
