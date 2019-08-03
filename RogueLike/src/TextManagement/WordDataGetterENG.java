f4bfb6b33bc5e109e36e38455f0af2431dcd11f4
JorViteri <rorschacht14@gmail.com>
2019-08-03 13:21:40 +0200

    Implementated the Getter for English

--- RogueLike/src/TextManagement/WordDataGetterENG.java
+++ RogueLike/src/TextManagement/WordDataGetterENG.java
@@ -2,6 +2,8 @@ package TextManagement;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
@@ -34,7 +36,7 @@ public class WordDataGetterENG implements WordDataGetter {
 		ArrayList<String> names = new ArrayList<>();
 		String aux;
 		int j;
-		JSONArray objectNames=null;
+		JSONObject objectNames=null;
 		JSONObject object = null;
 		Random random = new Random();
 		noun= noun.replace(" ", "_");
@@ -42,23 +44,20 @@ public class WordDataGetterENG implements WordDataGetter {
 			File file = new File("res/Synsets/ENG_Synsets/ENG_names_json.json");
 			String content = FileUtils.readFileToString(file,"utf-8");	
 			object= new JSONObject(content);
-			objectNames = object.getJSONArray(noun);
+			objectNames = object.getJSONObject(noun);
 		} catch(Exception e){
 			boolean c = true;
 			e.printStackTrace();
 		}		
-		if (objectNames != null) {
-			for (int i = 0; i < objectNames.length(); i++) {
-				String key = objectNames.getJSONObject(i).keySet().toString();
-				names.add(key);
-			}
-		}
+		
+		Collection<String> keys = objectNames.keySet();
+		names = new ArrayList<String>(keys);
 		
 		j=random.nextInt(names.size());
 		aux = names.get(j); 
 		aux = aux.replace("[", "");
 		aux = aux.replace("]", "");
-		object = objectNames.getJSONObject(j);
+		object = objectNames.getJSONObject(aux);
 		object = object.getJSONObject(aux);
 		aux =  aux.replace("_", " ");
 		result.add(0, aux);
@@ -70,78 +69,80 @@ public class WordDataGetterENG implements WordDataGetter {
 	@Override
 	public ArrayList<String> getAdjData(String adj, String genere) {
 		ArrayList<String> results = new ArrayList<>();
-		String key, adj_sing, adj_plu, aux;
-		ArrayList<String> adjectives = new ArrayList<>();
 		JSONArray objectAdjArr=null;
-		JSONObject adjectiveObj = null;
 		JSONObject object = null;
 		Random random = new Random();
-		int i;
-		
-		adj= adj.replace(" ", "_");
-		switch (genere){
-		case "femenine":
-			adj_sing = "sing_f";
-			adj_plu = "plu_f";
-			break;
-		case "masculine":
-			adj_sing = "sing_m";
-			adj_plu = "plu_m";
-			break;
-		default:
-			adj_sing = "sing_f";
-			adj_plu = "plu_f";
-		}
-		try{ //TODO creo que va a ser mejor hacer de esta una clase general para varios usos y que segun la funcion tendra un objeto u otro
-			File file = new File("res/Synsets/ENG_Synsets/ENG_adjectives_json.json");
+		int pos;
+		try{
+			File file = new File("res/Synsets/ENG_Synsets/ENG_names_json.json");
 			String content = FileUtils.readFileToString(file,"utf-8");	
 			object= new JSONObject(content);
 			objectAdjArr = object.getJSONArray(adj);
 		} catch(Exception e){
 			boolean c = true;
 			e.printStackTrace();
-		}	
-		
-		if (objectAdjArr != null) { //TODO en principio coge la llave lo cual MAL, deberia obtener la forma para la situaciion adecuada
-			for (i = 0; i < objectAdjArr.length(); i++) {
-				key = objectAdjArr.getJSONObject(i).keySet().toString();
-				adjectives.add(key);
-			}
-		}
-		i = random.nextInt(adjectives.size());
-		key= adjectives.get(i);
-		adjectiveObj = objectAdjArr.getJSONObject(i); //wut
-		key = key.replace("[", "");
-		key = key.replace("]", "");
-		adjectiveObj = adjectiveObj.getJSONObject(key);
-		
-		
-		aux =  adjectiveObj.getString(adj_sing); 
-		aux =  aux.replace("_", " ");
-		aux = aux.replace("[", "");
-		aux = aux.replace("]", "");
-		results.add(0,aux);
-		
-		aux =  adjectiveObj.getString(adj_plu);  
-		aux =  aux.replace("_", " ");
-		aux = aux.replace("[", "");
-		aux = aux.replace("]", "");
-		results.add(1,aux);
-		
+		}		
+		pos = random.nextInt(objectAdjArr.length());
+		results.add(objectAdjArr.getString(pos));
 		return results;
-		
 	}
 
 	@Override
 	public ArrayList<String> getVerbData(String verb) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> result = new ArrayList<>();
+		ArrayList<String> verbs = new ArrayList<>();
+		String aux;
+		int j;
+		JSONObject objectVerbs=null;
+		JSONObject object = null;
+		Random random = new Random();
+		verb= verb.replace(" ", "_");
+		try{
+			File file = new File("res/Synsets/ENG_Synsets/ENG_verbs_json.json");
+			String content = FileUtils.readFileToString(file,"utf-8");	
+			object= new JSONObject(content);
+			objectVerbs = object.getJSONObject(verb);
+		} catch(Exception e){
+			boolean c = true;
+			e.printStackTrace();
+		}		
+		
+		Collection<String> keys = objectVerbs.keySet();
+		verbs = new ArrayList<String>(keys);
+		
+		j=random.nextInt(verbs.size());
+		aux = verbs.get(j); 
+		aux = aux.replace("[", "");
+		aux = aux.replace("]", "");
+		object = objectVerbs.getJSONObject(aux);
+		object = object.getJSONObject(aux);
+		aux =  aux.replace("_", " ");
+		result.add(0, aux);
+		result.add(1,object.getString("ThirdPerson"));
+		result.add(2, object.getString("Past"));
+		return result;
 	}
 
 	@Override
 	public ArrayList<String> getAdvData(String adv) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> results = new ArrayList<>();
+		JSONArray objectAdvArr=null;
+		JSONObject object = null;
+		Random random = new Random();
+		int pos;
+		try{
+			File file = new File("res/Synsets/ENG_Synsets/ENG_adverbs_json.json");
+			String content = FileUtils.readFileToString(file,"utf-8");	
+			object= new JSONObject(content);
+			objectAdvArr = object.getJSONArray(adv);
+		} catch(Exception e){
+			boolean c = true;
+			e.printStackTrace();
+		}		
+		pos = random.nextInt(objectAdvArr.length());
+		results.add(objectAdvArr.getString(pos));
+		return results;
 	}
+	
 
 }
